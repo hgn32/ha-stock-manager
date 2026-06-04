8cb4f29072097c1375ab48eaa51aadf417e8008f
hgn32 <yuukies@gmail.com>
2026-06-04 23:53:53 +0900

    Default integration URL to add-on internal hostname (Ingress-only add-on)
    The Stock Manager add-on no longer publishes port 8099 to the LAN (Ingress
    only), so localhost:8099 is unreachable. Point the default at the add-on's
    internal hostname and document it in the README.

--- custom_components/stock_manager/const.py
+++ custom_components/stock_manager/const.py
@@ -1,5 +1,5 @@
 DOMAIN = "stock_manager"
 CONF_URL = "url"
 CONF_SCAN_INTERVAL = "scan_interval"
-DEFAULT_URL = "http://localhost:8099"
+DEFAULT_URL = "http://3a30c8ec-stock-manager:8099"
 DEFAULT_SCAN_INTERVAL = 300  # seconds
